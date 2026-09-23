8ec3157b3cb2616c90a51e2aef7057d56db2fba2
farroo <farwaabbas944@gmail.com>
2026-09-23 04:33:48 -0700

    Change greeting word to Greetings

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ class GreeterHandler(BaseHTTPRequestHandler):
         self.send_header('Content-type', 'text/html')
         self.end_headers()
        # Added a second line to the greeting
-        message = "<h1>Hello from Farwa!</h1><p>Welcome to my containerized app assignment!</p>"
+        message = "<h1>Greetings from Farwa!</h1><p>Welcome to my containerized app assignment!</p>"
         self.wfile.write(message.encode('utf-8'))
 
 
